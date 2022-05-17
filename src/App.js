42962ce8e377831ef36e00b472396c309d67ea61
yashv521 <55803892+yashv521@users.noreply.github.com>
2022-05-17 21:33:20 +0530

    3rd commit

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import {BrowserRouter as Router, Routes, Route} from
 import {Toastcontainer} from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css';
 import Navbar from './components/Navbar'
+import PrivateRoute from './components/PrivateRoute'
 import Explore from './pages/Explore'
 import Offers from './pages/Offers'
 import Profile from './pages/Profile'
@@ -19,7 +20,9 @@ function App() {
       <Routes>
         <Route path='/' element={<Explore />} />
         <Route path='/offers' element={<Offers />} />
+        <Route path='/profile' element={<PrivateRoute />}>
         <Route path='/profile' element={<Profile />} />
+        </Route>
         <Route path='/sign-in' element={<SignIn />} />
         <Route path='/sign-up' element={<SignUp />} />
         <Route path='/forgot-password' element={<ForgotPassword />} />
--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,11 +1,17 @@
-import { useState, useEffect } from 'react'
-import {getAuth} from 'firebase/auth'
+import { useState, useEffect, useRef } from 'react'
+import {getAuth, updateProfile} from 'firebase/auth'
+import {updateDoc, doc} from 'firebase/firestore'
+import { db } from '../firebase.config'
 import { useNavigate, Link } from 'react-router-dom'
+import { async } from '@firebase/util'
+import {toast} from 'react-toastify'
+
 
 
 
 function Profile() {
     const auth = getAuth()
+    const[changeDetails, setChangeDetails] = useState(false)
     const [formData, setFormData] = useState({
         name: auth.currentUser.displayName,
         email: auth.currentUser.email
@@ -18,7 +24,36 @@ function Profile() {
         auth.signOut()
         navigate('/')
     }
+
+    const onSubmit = async () => {
+        try {
+            if(auth.currentUser.displayName !==name)    {
+            //update display name in fb
+            await updateProfile(auth.currentUser, {
+                displayName: name
+            })
+
+            //update in firestore
+            const userRef = doc(db, 'users',auth.currentUser.uid)
+            await updateDoc(userRef, {
+                name, 
+            })
+                
+            }
+            
+        } catch (error) {
+            console.log(error)
+            toast.error('Could not update Profile Details')
+        }
+    }
     
+    const onChange = (e) => {
+        setFormData((prevState) => ({
+         ...prevState,
+           [e.target.id]: e.target.value,
+        }   ))
+        }
+
     return <div className='profile'>
         <header className="profileHeader">
             <p className="pageHeader">My Profile</p>
@@ -28,6 +63,38 @@ function Profile() {
                  LogOut
              </button>
         </header>
+
+        <main>
+            <div className="profileDetailsHeader">
+                <p className="profileDetailsText">Personal Details</p>
+                <p className="changePersonalDetails" onClick={() =>{
+                    changeDetails && onSubmit()
+                    setChangeDetails((prevState) => !prevState)
+                }}>
+                    {changeDetails ? 'done' : 'change'}
+                </p>
+            </div>
+            
+            <div className="profileCard">
+                <form>
+                <input  type="text" 
+                    id="name" 
+                    className={!changeDetails ? 'profileName' : 'profileNameActive'} 
+                    disabled={!changeDetails}
+                    value={name}
+                    onChange={onChange}
+                    /> 
+                    <input type="text" 
+                    id="email" 
+                    className={!changeDetails ? 'profileEmail' : 'profileEmailActive'} 
+                    disabled={!changeDetails}
+                    value={email}
+                    onChange={onChange}
+                    />    
+                </form>
+            </div>
+        </main>
+
         </div> 
 }   
 
