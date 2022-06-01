35e42d276656132393928c20411087ad90242457
yashv521 <55803892+yashv521@users.noreply.github.com>
2022-06-01 13:35:44 +0530

    10th commit map work

--- src/pages/Listing.jsx
+++ src/pages/Listing.jsx
@@ -1,11 +1,11 @@
 import { useState, useEffect } from "react";
 import { Link, useNavigate, useParams } from "react-router-dom";
+import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
 import { getDoc, doc } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 import { db } from "../firebase.config";
 import Spinner from "../components/Spinner";
 import shareIcon from "../assets/svg/shareIcon.svg";
-import { async } from "@firebase/util";
 
 function Listing() {
   const [listing, setListing] = useState(null);
@@ -88,7 +88,23 @@ function Listing() {
           <li>{listing.furnished && "Furnished"}</li>
           <p className="listingLocationTitle">Location</p>
 
-          {/* MAP */}
+          <div className="leafletContainer">
+            <MapContainer
+              style={{ height: "100%", width: "100%" }}
+              center={[listing.geolocation.lat, listing.geolocation.lng]}
+              zoom={13}
+              scrollWheelZoom={false}
+            >
+              <TileLayer attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+              url='https://{s}.tile.openstreetmap.de/tiles/osmde/{z}/{x}/{y}.png' />
+
+          <Marker position={[listing.geolocation.lat, listing.geolocation.lng]}>
+            <Popup>{listing.location}</Popup>
+          </Marker>
+
+              
+            </MapContainer>
+          </div>
 
           {auth.currentUser?.uid !== listing.userRef && (
             <Link
